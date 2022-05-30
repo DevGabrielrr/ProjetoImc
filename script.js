b0ec45dff7709ed79e300313fb001e2f4f8a8c60
Gabriel Ribeiro <95862666+DevGabrielrr@users.noreply.github.com>
2022-05-30 18:15:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,30 @@ var result = document.getElementById('resultado');
 
 
 if(res < 18.5){
-    result.innerHTML = "Seu IMC é:   " + res + "    Abaixo do peso! ";
+    result.innerHTML = "Olá" +nome+ ",Seu IMC é:   " + res + "    Abaixo do peso! ";
         
 
     }else if(res >= 18.6 && res <= 24.9){
-        result.innerHTML= "Seu IMC é:  " + res +  "     Peso ideal, Parabéns! ";
+        result.innerHTML= "Olá "+nome+ ",Seu IMC é:  " + res +  "     Peso ideal, Parabéns! ";
      
 
         }else if(res >= 25.0 && res <= 29.9 ){
-             result.innerHTML = "Seu IMC é:  " + res + "  Levemente acima do Peso! ";
+             result.innerHTML = "Olá " +nome+ ",Seu IMC é:  " + res + "  Levemente acima do Peso! ";
             
                 
             }else if(res >= 30.0 && res <= 34.9){
-                 result.innerHTML = "Seu IMC é:  " + res + "   Obesidade Grau I,Atenção! ";
+                 result.innerHTML = "Olá " +nome+ ",Seu IMC é:  " + res + "   Obesidade Grau I,Atenção! ";
                    
 
                 }else if(res >= 35.0 && res <= 39.9){
-                     result.innerHTML= "Seu IMC é:  " + res + "    Obesidade grau II,Cuidado com sua saúde!";
+                     result.innerHTML= "Olá "+nome+ ",Seu IMC é:  " + res + "    Obesidade grau II,Cuidado com sua saúde!";
                        
 
                     }else if(res >= 40.0){
-                         result.innerHTML = "Seu IMC é:  " + res + "   Obesidade Grau III, Você Precisa imediatamente fazer uma dieta!";
+                         result.innerHTML = "Olá "+nome+ ",Seu IMC é:  " + res + "   Obesidade Grau III, Você Precisa imediatamente fazer uma dieta!";
                       
+                    }else {
+                         result.innerHTML = "Verifique seus dados Novamente!";
                     }
 
 
